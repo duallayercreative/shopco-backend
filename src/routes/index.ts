14038f2b0646c7d9eca53de18a170f796317b728
Captain-Kanak <kanakroy835@gmail.com>
2026-07-11 16:10:49 +0600

    successfully product add done

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authRouter } from "../modules/auth/auth.route.js";
 import { categoryRouter } from "../modules/category/category.route.js";
 import { brandRouter } from "../modules/brand/brand.route.js";
+import { productRouter } from "../modules/product/product.route.js";
 
 const router: Router = Router();
 
@@ -10,5 +11,7 @@ router.use("/auth", authRouter);
 router.use("/brands", brandRouter);
 
 router.use("/categories", categoryRouter);
+
+router.use("/products", productRouter);
 
 export { router as indexRouter };
